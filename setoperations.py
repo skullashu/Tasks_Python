cb8009f7178ea8538bec98ec0a40495538fe7ac8
skullashu <86848846+skullashu@users.noreply.github.com>
2021-11-01 10:00:44 +0530

    Create setoperations.py

---
+++ setoperations.py
@@ -0,0 +1,18 @@
+E = {0,1,2,3,4,5}
+N = {3,4,5,6,7,8,9}
+
+#Union of sets
+Z = E.union(N)
+print(Z)
+
+#Intersection of sets
+Y = E.difference(N)
+print(Z)
+
+#Difference of sets
+R = E.difference(N)
+print(R)
+
+#Symmetric difference of sets
+D = E.symmetric_difference(N)
+print(D)
